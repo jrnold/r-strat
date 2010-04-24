dffe4925b7bd56fba57db8504a7f68177013432d
Jeffrey.Arnold <jeffrey.arnold@gmail.com>
2010-04-23 21:01:05 -0400

    more housecleaning before new start

---
+++ R/strat.R
@@ -0,0 +1,23 @@
+##' <description>
+##'
+##' <details>
+##' @title
+##' @param y
+##' @param U
+##' @param agentError
+##' @param privateInfo
+##' @param data
+##' @param ...
+##' @return
+##' @author Jeffrey Arnold
+##'
+strat <- function(y,
+                  U=list(),
+                  agentError=0,
+                  privateInfo=1,
+                  data=NULL,
+                  ...
+                  )
+{
+
+}
